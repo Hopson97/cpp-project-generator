cc2c177a1d8e7247c3700d10c92919e5688c4275
Hopson97 <mhopson@hotmail.co.uk>
2019-07-30 22:16:51 +0100

    improve the deploy script

--- templates/deploy.sh
+++ templates/deploy.sh
@@ -1,11 +1,12 @@
 sh scripts/build.sh release 
 
+rm -rf -d <PNAME>
 
 mkdir <PNAME>
 mkdir <PNAME>/res
 
 cp bin/release/<PNAME> <PNAME>
-cp -r -d res <PNAME>/res
+cp -r res <PNAME>/res
 
 echo "Deploy build created."
 echo "cd <PNAME> to find it"
