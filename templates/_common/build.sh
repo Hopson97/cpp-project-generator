d16f586856a875b6b25be42a171ed7b3ef2a6dbb
Hopson97 <mhopson@hotmail.co.uk>
2021-05-09 19:49:28 +0100

    Add stb dep

--- templates/_common/build.sh
+++ templates/_common/build.sh
@@ -1,15 +1,11 @@
 #!/bin/bash
 
-mkdir -p bin
-cd bin 
-mkdir -p release
-mkdir -p debug
-
 target_release() {
     cd release
     cmake -DCMAKE_BUILD_TYPE=Release ../..
     make
     echo "Built target in bin/release/"
+    cd ../..
 }
 
 target_debug() {
@@ -17,15 +13,35 @@ target_debug() {
     cmake -DCMAKE_BUILD_TYPE=Debug ../..
     make
     echo "Built target in bin/debug/"
+    cd ../..
 }
 
+# Create folder for distribution
+if [ "$1" = "release" ]
+then
+    if [ -d "$<PNAME> " ]
+    then
+        rm -rf -d <PNAME> 
+    fi
+
+    mkdir -p <PNAME> 
+fi
+
+# Creates the folder for the binaries
+mkdir -p <PNAME>
+mkdir -p <PNAME>/Data
+mkdir -p bin
+mkdir -p bin/release
+mkdir -p bin/debug
+cd bin
+
+# Builds target
 if [ "$1" = "release" ]
 then
     target_release
-elif [ "$1" = "both" ]
-then
-    target_release
-    target_debug
+    cp bin/release/<PNAME>  <PNAME>/<PNAME> 
 else
     target_debug
 fi
+
+cp -R Data <PNAME>/Data
